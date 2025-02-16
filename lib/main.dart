77561a6e629342e2e564546d88a5c769983db58c
m00n3r-dev <jeewakasandeep2021@gmail.com>
2025-02-17 00:04:31 +0530

    feat: initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,14 @@ import 'package:myapp/profile.dart';
 import 'package:go_router/go_router.dart';
 import 'package:myapp/routes/router.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApp(),
   );
