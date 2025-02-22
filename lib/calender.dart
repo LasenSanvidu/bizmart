f84749798b2c6dbd6d2d0cab3d4ecdd797503c55
Shashikirana531 <shashikirana.20221976@iit.ac.lk>
2025-02-22 22:34:18 +0530

    modified calender page

--- lib/calender.dart
+++ lib/calender.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:myapp/cal_event_page';
 
 class CalendarPage extends StatefulWidget {
@@ -21,43 +22,56 @@ class _CalendarPageState extends State<CalendarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            context.go("/");
+          },
+        ),
+        title: const Text(
           " Calendar",
           style: TextStyle(
             color: Colors.black,
-            fontSize: 30,
-            backgroundColor: Colors.purple[100],
+            fontSize: 29.0,
+            fontWeight: FontWeight.bold,
           ),
         ),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () async {
-              final event = await Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => EventFormPage()),
-              );
-              if (event != null) {
-                addEvent(event);
-              }
-            },
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () async {
+                final event = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EventFormPage()),
+                );
+                if (event != null) {
+                  addEvent(event);
+                }
+              },
+            ),
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: events.length,
-        itemBuilder: (context, index) {
-          final event = events[index];
-          return Card(
-            color: Colors.purple[100],
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: ListTile(
-              title: Text("${event['title']}"),
-              subtitle: Text(
-                  "📅 ${event['date']}  |  Duration: ${event['duration']} day(s)"),
-            ),
-          );
-        },
+      body: Container(
+        color: const Color.fromARGB(
+            255, 248, 246, 255), // Set the background color to a light purple
+        child: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            final event = events[index];
+            return Card(
+              color: const Color.fromARGB(255, 245, 221, 249),
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: ListTile(
+                title: Text("${event['title']}"),
+                subtitle: Text(
+                    " ${event['date']}  |  Duration: ${event['duration']} day(s)"),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
