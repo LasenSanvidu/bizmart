841d0d0c533461fde0ea237600ca0a00e228b13e
Shashikirana531 <shashikirana.20221976@iit.ac.lk>
2025-02-23 18:51:39 +0530

    fix some issues

--- lib/calender.dart
+++ lib/calender.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:myapp/cal_event_page';
+import 'package:myapp/cal_event_page.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,28 +1,23 @@
 import 'package:go_router/go_router.dart';
-import 'package:myapp/chat.dart';
+import 'package:myapp/cal_event_page.dart';
 import 'package:myapp/chat_homeScreen.dart';
 import 'package:myapp/Register.dart';
-import 'package:myapp/cal_event_page';
+
 import 'package:myapp/calender.dart';
 import 'package:myapp/login.dart';
 import 'package:myapp/main.dart';
 import 'package:myapp/main_settings.dart';
-import 'package:myapp/order_confirmed.dart';
-import 'package:myapp/profile.dart';
 import 'package:myapp/settings_customer.dart';
 import 'package:myapp/user_type_selection.dart';
-import 'package:myapp/welcomesecond.dart';
-import '../otp_code.dart';
-import '../otp_confirmation.dart';
 
 class RouterClass {
   final router = GoRouter(
-    initialLocation: "/main",
+    initialLocation: "/",
     routes: [
       GoRoute(
-        path: "/calendar",
+        path: "/",
         builder: (context, state) {
-          return const CalendarPage();
+          return const HomeScreen();
         },
       ),
        GoRoute(
@@ -62,9 +57,9 @@ class RouterClass {
           },
       ),
        GoRoute(
-        path: "/eventForm",
+        path: "/chat",
         builder: (context, state) {
-          return const EventFormPage();
+          return const ChatHomeScreen();
         },
       ),
     ],
