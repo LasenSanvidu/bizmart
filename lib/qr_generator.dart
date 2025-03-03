838d9d1e98d9ba05b5eaa33de7b75ee2ea9e6ca2
dinadag <dinadagunasekara90@gmail.com>
2025-03-03 15:11:05 +0530

    Added navigation, actions, and bottom navigation bar Added the right dependancies with the correction of QrImageView with QrImage

--- lib/qr_generator.dart
+++ lib/qr_generator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QRCodeScreen extends StatelessWidget {
   const QRCodeScreen({super.key});
@@ -7,12 +8,40 @@ class QRCodeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xFFD1C4E9), // Light purple
+        backgroundColor: const Color(0xFFD1C4E9),
         title: const Text(
           'SoleCraft',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
-      )
-    )
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: QrImageView(
+          data: "https://example.com",
+          size: 200.0,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.purple,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.qr_code), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+        ],
+      ),
+    );
   }
 }
